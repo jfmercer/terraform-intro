12bc7f8b2c8a225135c663b7435445f3ea3ca505
John F. Mercer <jfmercer@protonmail.com>
2020-01-26 10:51:23 -0500

    Add S3 bucket

--- example.tf
+++ example.tf
@@ -12,3 +12,11 @@ resource "aws_eip" "ip" {
     vpc = true
     instance = aws_instance.example.id
 }
+
+resource "aws_s3_bucket" "example" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
+  # this name must be changed before applying this example to avoid naming
+  # conflicts.
+  bucket = "terraform-getting-started-guide-124505c"
+  acl    = "private"
+}
