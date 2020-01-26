f2390102f439169031c491c8aac43e8003e1e6dc
John F. Mercer <jfmercer@protonmail.com>
2020-01-26 12:54:46 -0500

    Move vars to .tf file

--- example.tf
+++ example.tf
@@ -1,3 +1,14 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami-b374d5a5" {
+  default = "ami-b374d5a5"
+}
 provider "aws" {
   profile    = "default"
   region     = "var.region"
