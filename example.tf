0ef2d8e0e0d55b5095a5f70fdfdbfd1e721395db
John F. Mercer <jfmercer@protonmail.com>
2020-01-26 13:13:12 -0500

    Revert "Move vars to .tf file"
    This reverts commit f2390102f439169031c491c8aac43e8003e1e6dc.

--- example.tf
+++ example.tf
@@ -1,14 +1,3 @@
-variable "region" {
-  default = "us-east-2"
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "ami-b374d5a5" {
-  default = "ami-b374d5a5"
-}
 provider "aws" {
   profile    = "default"
   region     = "var.region"
---
+++ terraform.tfvars
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami-b374d5a5" {
+  default = "ami-b374d5a5"
+}
